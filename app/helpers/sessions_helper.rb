9b141d1f03efaaf40b8ad1f0152051c701a17ce8
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-18 08:41:54 +0000

    Done.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  def session_name
+    session[:name]
+  end
+
+  def is_logged_in
+    if session[:name].present?
+      true
+    else
+      false
+    end
+  end
+
 end
