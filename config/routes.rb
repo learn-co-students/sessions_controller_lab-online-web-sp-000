bac266aee4a1c4ae5c17eb3bd1cb2f56557f2a06
Brandon Teich <brandonteich123@gmail.com>
2019-10-20 21:55:36 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+<<<<<<< HEAD
+  
+=======
+>>>>>>> fc518f99d6cf4e97a70bca77dae606e0b5353c58
   get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
