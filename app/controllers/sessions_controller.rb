27c80f8d3fde3d3488ee973c222adf0a8c3a8aea
jei-doubleton <jennifer.singleton@gmail.com>
2019-12-03 19:06:05 -0600

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class  SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        if !params[:name] || params[:name].empty?
+            redirect_to '/login' 
+            return
+        end
+        
+        session[:name] = params[:name]
+        redirect_to '/'
+    end
+
+    def destroy
+        session.delete :name
+        redirect_to '/login'
+    end
+end
