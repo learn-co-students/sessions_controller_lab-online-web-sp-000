fadb32ab55f454a00f7facea9b45f91cbeb605f6
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-29 21:38:58 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    return redirect_to action: 'new' if params[:name].nil? || params[:name].empty?
+    return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
-    redirect_to '/'
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def destroy
     session.delete :name
-    redirect_to '/'
+    redirect_to controller: 'application', action: 'hello'
   end
 
 end
