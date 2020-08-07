2a0ce9987dcc54e45cb354b95a9872e7c2dbc406
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-06 23:45:15 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController 
+
+    def new 
+    end
+
+    def create 
+        if params[:name] == ''
+            redirect_to '/login'
+        elsif    
+            params[:name]
+            session[:name] = params[:name]
+
+            redirect_to '/'
+        else
+            redirect_to '/login'
+        end
+    end
+
+    def destroy
+        session.delete :name
+    end
+
+end
