810f66b9f08950be8417ceca9e0382bd68c638c4
chienleow <chienleow1@gmail.com>
2020-08-20 22:19:45 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if params[:name].blank? || params[:name].nil?
+        if params[:name].blank? # blank? is not a standard Ruby method but is added to all objects by Rails and returns true for nil, false, empty, or a whitespace string.
             redirect_to '/login'
         else
             session[:name] = params[:name]
