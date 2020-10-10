394311420908e5a08c3e8e9fed25cd7f2c7acee4
mdacht777 <mdacht777@gmail.com>
2020-10-10 09:41:05 -0600

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+    def new
+        # nothing to do here!
+    end
+ 
+    def create
+        if params[:name].nil? or params[:name]==""
+          redirect_to '/login'
+        else
+          session[:name] = params[:name]
+          redirect_to '/'
+        end
+    end
+
+    def destroy
+      session.delete :name
+    end
+end
