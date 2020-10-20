0b000a92fade39f171b2395d0c3ed4cb75148c09
yarvinh <yarvinhhernandez@gmail.com>
2020-10-20 12:44:32 -0400

    Done.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+    def username(username)
+        if username != nil
+            if username.empty?
+                "empty"
+            else
+                true
+            end
+        else
+            false
+        end
+    end
 end
