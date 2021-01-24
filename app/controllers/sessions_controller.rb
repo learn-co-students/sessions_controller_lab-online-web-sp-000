6f438f3341b65455c878dde20a95c24bfb603342
Chad Lengefeld <chadlengefeld@gmail.com>
2021-01-24 07:57:32 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,16 @@ class SessionsController < ApplicationController
 
   def create
     session[:name] = params[:name]
-    if !!params[:name]
-      
-    #   redirect_to 'sessions/new'
-    # else
+      if !params[:name]
+        redirect_to '/sessions/new'
+
+    elsif params[:name] = nil || params[:name].empty?
+       redirect_to '/'
+    else
+    # if params[:name].nil?
     #   params[:name].empty? || !params[:name]
       redirect_to '/'
+
     end
   end
 
