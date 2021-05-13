8314ce135c4e44a68b093eedf1a31b15b0628aff
Christerle Nounoute <christerlesn@gmail.com>
2021-05-13 18:23:41 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if session[:name] = params[:name]
+      redirect_to "/"
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+  end
+end
