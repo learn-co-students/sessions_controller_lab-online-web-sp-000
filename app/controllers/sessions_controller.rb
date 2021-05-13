6b0353d15bb89e453b0feb96e071bfd9fc44fa86
Christerle Nounoute <christerlesn@gmail.com>
2021-05-13 18:53:03 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if session[:name] = params[:name]
+    if params[:name] == nil || params[:name].empty?
+      redirect_to "/login"
+    elsif session[:name] = params[:name]
       redirect_to "/"
-    elsif session[:name].empty?
-      redirect_to '/login'
     else
-      redirect_to '/login'
+      redirect_to "/"
     end
   end
 
   def destroy
+    if session[:name] = params[:name]
+      session[:name].delete
+    else
+    end
   end
 end
